afb35c7a8323668c12ebad6350d066b775102393
LMK354 <lmksorry472@gmail.com>
2025-05-10 12:13:49 +0200

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -55,7 +55,7 @@ cmd({
                 return await conn.sendMessage(
                     from,
                     {
-                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/7zfdcq.jpg' },
+                        image: { url: config.MENU_IMAGE_URL || 'https://i.ibb.co/dsGhfnnV/bmb-xmd.jpg' },
                         caption: menuCaption,
                         contextInfo: contextInfo
                     },
@@ -423,7 +423,7 @@ cmd({
                                 await conn.sendMessage(
                                     senderID,
                                     {
-                                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/7zfdcq.jpg' },
+                                        image: { url: config.MENU_IMAGE_URL || 'https://i.ibb.co/dsGhfnnV/bmb-xmd.jpg' },
                                         caption: selectedMenu.content,
                                         contextInfo: contextInfo
                                     },
